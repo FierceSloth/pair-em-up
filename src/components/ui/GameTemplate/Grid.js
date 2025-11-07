dfe1dd511b5adf5189e781ee302a6c3c526cc00d
Dastan Hairushev <hairushevdastan@gmail.com>
2025-11-07 22:47:13 +0500

    feat: add basic design to game

--- src/components/ui/GameTemplate/Grid.js
+++ src/components/ui/GameTemplate/Grid.js
@@ -1,6 +1,5 @@
 import Component from '../../../utils/Component';
 import { emitter } from '../../../utils/emmiter';
-import getRandomInt from '../../../utils/random';
 import Cell from './Cell';
 
 const CLASSIC_GRID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1, 9];
@@ -27,7 +26,7 @@ export default class Grid extends Component {
 
   createBasicGrid() {
     this.cells = CLASSIC_GRID.map((value, index) => {
-      const cell = new Cell({value: value, index: index});
+      const cell = new Cell({ value: value, index: index });
       this.append(cell);
       return cell;
     });
